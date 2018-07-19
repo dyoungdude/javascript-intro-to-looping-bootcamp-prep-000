bff602664fc02faf4425e38a1ce309948a256c97
Darren Young <dlyoung5@asu.edu>
2018-07-19 03:23:24 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,37 @@
+function forLoop(array){
+  var array = [];
+  for(let i = 0; i < 25; i++){
+    if(i === 1){
+      array.push('I am ' + i + ' strange loop.');
+    }
+    else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array
+}
+
+function whileLoop(n){
+  let number = n;
+  return number;
+}
+
+while (n > 0){
+  --whileLoop();
+  console.log(--whileLoop);
+  if (n === 0){
+    console.log('done');
+  }
+}
+
+function doWhileLoop(array){
+  var array = [];
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  return array;
+}
+
+do{
+  --doWhileLoop()
+}while(array.length > 0 && maybeTrue())
